42729b815d2f22a950ae379b2301a600163519eb
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-05-27 22:02:09 -0600

    “Vuetify-Components2”

--- src/App.vue
+++ src/App.vue
@@ -3,49 +3,31 @@
     <v-app-bar app color='primary' dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+        <v-btn 
+          v-for='link in links'
+          :key="`${link.label}-header-link`"
+          text
+          rounded 
+          :to='link.url'
+        >
+      {{link.label}}
+      </v-btn>
     </v-app-bar>
     <v-content>
-      <!-- Login Module -->
-      <v-card width="400px" class="mt-5 mx-auto" dark>
-        <v-card-title class='pb-3'> 
-          <h1>Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field 
-              label='Username' 
-              prepend-icon="mdi-account-circle"
-            />
-            <v-text-field 
-              label='Password' 
-              :type="showPassword ? 'text':'password' " 
-              prepend-icon="mdi-lock" 
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color='success'>Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color='info'>Login</v-btn>
-        </v-card-actions>
-      </v-card>
+     <router-view></router-view>
     </v-content>
     <v-footer color="primary lighten-1" padless>
       <v-layout justify-center wrap>
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to='link.url'
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-flex primary lighten-2 py-4 text-center white--text xs12>
           {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
@@ -60,10 +42,19 @@ export default {
   name: 'App',
   data (){
     return{
-      showPassword: false,
       links: [
-        'Home',
-        'Login'
+        {
+          label: 'Home',
+          url: '/'
+        },
+        {
+          label: 'Login',
+          url: '/login'
+        },
+        {
+          label: 'Dashboard',
+          url: '/dashboard'
+        }
       ]
     }
   } 
